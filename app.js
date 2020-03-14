1bba4e36325ab30be3248f44a1b43e4fb18586c0
OmarHadhoud <OmarHadhoud@ymail.com>
2020-03-14 13:36:05 +0200

    Updated the app to call the user router

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //This file should include the names of the routes to be requested
+const express = require('express')
+const app = express()
+const usersRouter = require('./routes/users')
+const dotenv = require('dotenv')
+const mongoose= require('mongoose')
+
+dotenv.config()
+const mongoDB = process.env.MONGO_URI
+mongoose.connect(mongoDB, { useNewUrlParser : true, useUnifiedTopology : true})
+
+const db = mongoose.connection
+db.once('open', url => {
+    console.log('Database connected')
+  })
+  
+  db.on('error', err => {
+    console.error('connection error:', err)
+  })
+
+app.use(express.json())
+app.use('/', usersRouter)
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port : ` + process.env.PORT);
+})
