bc3044cb4c52597cf3a1d09ba23ca76224ded8be
OmarHadhoud <OmarHadhoud@ymail.com>
2020-03-18 12:23:44 +0200

    Deleted start.js as server.js does same funcionality to start server

--- start.js
+++
@@ -1,5 +0,0 @@
-//This file runs the server and starts it.
-const app = require('./app.js')
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on port : ` + process.env.PORT);
-})
