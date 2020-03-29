9348fc36561e839dda6c8ec482abec17e584178a
OmarHadhoud <OmarHadhoud@ymail.com>
2020-03-29 23:21:52 +0200

    Added the controlling function to add a track with a context to recently played for the current user

---
+++ controllers/playerController.js
@@ -0,0 +1,108 @@
+/**
+ * PlayerController module.
+ * @module playerController
+ * @requires express
+ */
+
+/**
+ * Player controller to call when routing.
+ * @type {object}
+ * @const
+ * @namespace playerController
+ */
+
+ /**
+ * express module
+ * Context model from the database
+ * @const
+ */
+const Context = require('../models/contextModel')
+/**
+ * express module
+ * Play History model from the database
+ * @const
+ */
+const PlayHistory = require('../models/playHistoryModel')
+/**
+ * express module
+ * Track model from the database
+ * @const
+ */
+const Track = require('../models/trackModel')
+
+/**
+ * express module
+ * Async functions
+ * @const
+ */
+const async = require('async')
+
+/**
+ * express module
+ * catchAsync utils file
+ * @const
+ */
+const catchAsync = require('../utils/catchAsync')
+
+/**
+ * express module
+ * APIFeatures utils file
+ * @const
+ */
+const APIFeatures = require('../utils/apiFeatures')
+
+
+/**
+ * express module
+ * Authroization controller
+ * @const
+ */
+const authController = require('../controllers/authController')
+
+
+/**
+ * express module
+ * error object
+ * @const
+ */
+const AppError = require('../utils/appError')
+
+/**
+ * A function that is used to add a track to the recently played list
+ * @memberof module:controllers/player~playerController
+ * @param {Request}  - The function takes the request as a parameter to access its body.
+ * @param {Respond} - The respond sent
+ * @param {done} - The next function in the middleware
+ */
+exports.addToRecentlyPlayed = catchAsync(async function (req, res, next) {
+    const userId = await authController.getUserId(req)
+
+    //Check if we reached the limit for this user
+    const count = await PlayHistory.countDocuments({'userId':userId})
+    if(count >= parseInt(process.env.PLAY_HISTORY_MAX_COUNT,10)){ //If we reached the limit of playHistory for this user
+        const oldestPlayHistory = await PlayHistory
+        .find()
+        .where('userId').equals(userId)
+        .sort('playedAt')
+        .limit(1)
+        await PlayHistory.remove({_id: {$in: oldestPlayHistory}}) //Remove the oldest play history object for this user
+    }
+
+    //TODO: Instead of getting the context from the request, we should have it saved 
+    // when the user started playing
+    const newContext = await Context.create(req.body.context)
+    const track = await Track.find().where('uri').equals(req.body.trackUri).select('_id')
+    if(!track){
+        return next(new AppError('No track with this uri was found!',404))
+    }
+    const newPlayHistory = new PlayHistory({
+        'userId':userId,
+        'context': newContext._id,
+        'playedAt': Date.now(),
+        'track': track[0]._id
+    })
+    await newPlayHistory.save()
+    
+    res.status(204).send()
+})
+
