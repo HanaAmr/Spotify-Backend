992af2f9aa4ef26f6072ba5bb55e32b99f26cedd
Hana <1999hanaamr@gmail.com>
2020-04-07 17:09:15 +0200

    Adding more seeds

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -62,7 +62,7 @@ exports.getAlbumsWithIds = catchAsync(async (req, res, next) => {
   const features = new APIFeatures(Album.find().where('_id').in(ids), req.query)
   const albums = await features.query.select('-__v').populate({
     path: 'artists',
-    select: '_id name uri href externalUrls images type followers userStats userArtist' // user public data
+    select: '_id name uri href externalUrls images role followers userStats artistInfo' // user public data
 
   })
 
@@ -90,7 +90,7 @@ exports.getAlbumsWithIds = catchAsync(async (req, res, next) => {
 exports.getOneAlbum = catchAsync(async (req, res, next) => {
   const album = await Album.findById(req.params.albumId).select('-__v').populate({
     path: 'artists',
-    select: '_id name uri href externalUrls images type followers userStats userArtist' // user public data
+    select: '_id name uri href externalUrls images role followers userStats artistInfo' // user public data
 
   })
 
@@ -120,7 +120,7 @@ exports.getAlbumTracks = catchAsync(async (req, res, next) => { //  non paginate
 
   const tracksArray = await features.query.select('-album -audioFilePath').populate({
     path: 'artists',
-    select: '_id name uri href externalUrls images type followers userStats artistInfo' // user public data
+    select: '_id name uri href externalUrls images role followers userStats artistInfo' // user public data
 
   })
 
@@ -165,7 +165,7 @@ exports.getSortedAlbums = catchAsync(async (req, res, next) => { //  not paginat
   const features = new APIFeatures(Album.find().select('-__v'), req.query).sort().paginate()
   const albums = await features.query.populate({
     path: 'artists',
-    select: '_id name uri href externalUrls images type followers userStats artistInfo' // user public data
+    select: '_id name uri href externalUrls images role followers userStats artistInfo' // user public data
 
   })
 
