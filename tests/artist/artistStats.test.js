4d832d1104adfb2cd20b9bd15a49c07f198f1416
nadakhalled <nadaelkhamy@gmail.com>
2020-06-10 23:01:37 +0200

    Fixed bug in artist viewing tests and added yearly&monthly stats tests

--- tests/artist/artistStats.test.js
+++ tests/artist/artistStats.test.js
@@ -52,12 +52,32 @@ const mongoDB = process.env.TEST_DATABASE
   mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 
   //Some dates that will be used later on in testing
+  //Dates for daily stats
   let todayDate=new Date(Date.now())
   todayDate.setUTCHours(0,0,0,0)
   let date1=new Date(todayDate)
   date1.setUTCDate(date1.getUTCDate()+1)
-  let date2=new Date(todayDate)
-  date2.setUTCDate(date2.getUTCDate()+2)
+  let date2=new Date(date1)
+  date2.setUTCDate(date2.getUTCDate()+1)
+
+  //Dates for monthly stats
+  let dateFeb=new Date(2020,1,13)
+  dateFeb.setUTCHours(0,0,0,0)
+  let date2Feb=new Date(2020,1,15)
+  date2Feb.setUTCHours(0,0,0,0)
+  let dateJan=new Date(2020,0,5)
+  dateJan.setUTCHours(0,0,0,0)
+  let dateDec=new Date(2019,11,24)
+  dateDec.setUTCHours(0,0,0,0)
+
+  //Dates for yearly stats
+  let date2020=new Date(2020,3,6)
+  date2020.setUTCHours(0,0,0,0)
+  let date2019=new Date(2019,5,4)
+  date2019.setUTCHours(0,0,0,0)
+  let date2017=new Date(2017,0,10)
+  date2017.setUTCHours(0,0,0,0)
+  
   
 
   // Testing shufflng queue list for a user.
@@ -264,7 +284,6 @@ describe('Testing calculating daily listens and likes statitics for albums and t
       })
       await track.save()
 
-      //adding album without any listens or likes history
       const album = new Album({
         _id:"5edefb5fd1537f3f33f91340",
         name: 'Sahran',
@@ -294,7 +313,6 @@ describe('Testing calculating daily listens and likes statitics for albums and t
 
     })
 
-    // Drop the whole users, playHistory collection after finishing testing
     afterAll(async () => {
       await mongoose.connection.collection('albums').deleteMany({})
       await mongoose.connection.collection('tracks').deleteMany({})
@@ -308,6 +326,7 @@ describe('Testing calculating daily listens and likes statitics for albums and t
         let dateTobeChecked= new Date(date2)
         expect(dailyStats[29].numberOfLikes).toEqual(1)
         expect(dailyStats[27].numberOfLikes).toEqual(4)
+        expect(dailyStats.length).toEqual(30)
         for(var i=29;i>-1;i--)
         {
             expect(dailyStats[i].day.getTime()).toEqual(dateTobeChecked.getTime())
@@ -321,6 +340,7 @@ describe('Testing calculating daily listens and likes statitics for albums and t
         artistServiceClass = new artistService()
         album=await Album.findById("5edefb5fd1537f3f33f91340")
         const dailyStats=await artistServiceClass.getDailyListensStats(album)
+        expect(dailyStats.length).toEqual(30)
         let dateTobeChecked= new Date(date2)
         for(var i=29;i>-1;i--)
         {
@@ -329,6 +349,128 @@ describe('Testing calculating daily listens and likes statitics for albums and t
         }
       })
   })
+
+
+    //Testing get Monthly and yealy likes/listens stats for album and track
+describe('Testing get Monthly and yearly likes/listens stats for album and track', () => {
+
+  beforeAll(async () => {
+    await mongoose.connection.collection('albums').deleteMany({})
+    await mongoose.connection.collection('tracks').deleteMany({})
+    sinon.restore()
+
+    const track = new Track({
+      _id:"5edefb60f3962c3f4257708f",
+      name: 'Youm Talat',
+      type: 'track',
+      externalUrl: 'this should be an externalUrl',
+      externalId: 'this should be an externalId',
+      trackNumber: 1,
+      durationMs: 212000,
+      popularity: 200000,
+      audioFilePath: 'tracks/track3.mp3',
+      listensHistory:[
+        {
+          day: date2017,
+          numberOfListens: 30000
+        },
+        {
+          day: date2019,
+          numberOfListens: 25000
+        },
+        {
+          day: date2020,
+          numberOfListens: 750
+        }]
+  
+    })
+    await track.save()
+
+    const album = new Album({
+      _id:"5edefb5fd1537f3f33f91340",
+      name: 'Sahran',
+      image: `${process.env.API_URL}/public/imgs/albums/Sahran.jpg`,
+      albumType: 'album',
+      externalUrls: 'this should be an externalUrl',
+      type: 'album',
+      genre: 'Arabic',
+      totalTracks: 14,
+      popularity: 400000,
+      likesHistory:[
+        {
+          day: dateDec,
+          userID: new mongoose.Types.ObjectId('5edeec8390345d81372ea819')
+        },
+        {
+            day: dateJan,
+            userID: new mongoose.Types.ObjectId('5edd8e684739973be7409f8d')
+          },
+          {
+            day: dateJan,
+            userID: new mongoose.Types.ObjectId('5edd0d2b97aa101fa834e42e')
+          },
+          {
+            day: dateFeb,
+            userID: new mongoose.Types.ObjectId('5ed158909065d5711e3caf13')
+          },
+          {
+            day: date2Feb,
+            userID: new mongoose.Types.ObjectId('5edcf97b98403513f44d2da6')
+          }
+        ]
+    })
+  await album.save()
+  })
+  afterAll(async () => {
+    await mongoose.connection.collection('albums').deleteMany({})
+    await mongoose.connection.collection('tracks').deleteMany({})
+  })
+
+  //Testing getting yearly listens stats for track
+  it('Testing getting yearly listens stats for track, stats object should have sequential dates', async () => {
+    artistServiceClass = new artistService()
+    let track=await Track.findById("5edefb60f3962c3f4257708f")
+    const yearlyStats=await artistServiceClass.getMonthlyOrYearlyStats(track,"yearly","listens")
+    let dateTobeChecked= new Date(date2020)
+    dateTobeChecked.setUTCDate(1)
+    dateTobeChecked.setUTCMonth(0)
+    //expect yearly listens to be with entered values
+    expect(yearlyStats.length).toEqual(5)
+    expect(yearlyStats[4].numberOfListens).toEqual(750)
+    expect(yearlyStats[3].numberOfListens).toEqual(25000)
+    expect(yearlyStats[1].numberOfListens).toEqual(30000)
+
+    for(var i=4;i>-1;i--)
+    {
+        expect(yearlyStats[i].day.getTime()).toEqual(dateTobeChecked.getTime())
+        dateTobeChecked.setUTCFullYear(dateTobeChecked.getUTCFullYear()-1)
+    }
+  })
+
+  //Testing getting monthly likes stats for album
+  it('Testing getting monthly likes stats for album, stats object should have sequential dates', async () => {
+
+    artistServiceClass = new artistService()
+    album=await Album.findById("5edefb5fd1537f3f33f91340")
+    const monthlyStats=await artistServiceClass.getMonthlyOrYearlyStats(album,"monthly","likes")
+    expect(monthlyStats.length).toEqual(12)
+    expect(monthlyStats[11].numberOfLikes).toEqual(2)
+    expect(monthlyStats[9].numberOfLikes).toEqual(1)
+    expect(monthlyStats[10].numberOfLikes).toEqual(2)
+
+    let dateTobeChecked= new Date(date2Feb)
+    dateTobeChecked.setUTCDate(1)
+
+    for(var i=11;i>-1;i--)
+    {
+        expect(monthlyStats[i].day.getTime()).toEqual(dateTobeChecked.getTime())
+        dateTobeChecked.setUTCMonth(dateTobeChecked.getUTCMonth()-1)
+    }
+  })
+
+
+})
+
 
    ///////////////////////////////
    /*
